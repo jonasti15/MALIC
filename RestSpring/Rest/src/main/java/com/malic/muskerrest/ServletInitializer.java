8430bd5d85f5751f6d2febcb39b6d3264a5a310d
estalyncuray <estalynsalvador.cura@alumni.mondragon.edu>
2022-05-21 00:29:07 +0200

    Rest -properties modificado correcta numenclatura y configuración db - pom, añadido tomcat y packaging

---
+++ RestSpring/Rest/src/main/java/com/malic/muskerrest/ServletInitializer.java
@@ -0,0 +1,17 @@
+package com.malic.muskerrest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * <p><b>Servlet Initializer</b></p>
+ * <p>Initializer of the Servlet for the web application.</p>
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MuskerRestApplication.class);
+    }
+
+}

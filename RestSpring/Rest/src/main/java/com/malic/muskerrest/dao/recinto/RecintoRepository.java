cf235bc417c869bd43e813397429094b5bcc5566
Ibai <ibai.lopez@alumni.mondragon.edu>
2022-06-02 10:50:39 +0200

    gitflow-feature-stash: TomarEnCuentaLasEstanciasParaRecintos

--- RestSpring/Rest/src/main/java/com/malic/muskerrest/dao/recinto/RecintoRepository.java
+++ RestSpring/Rest/src/main/java/com/malic/muskerrest/dao/recinto/RecintoRepository.java
@@ -9,6 +9,13 @@ import java.util.List;
 
 public interface RecintoRepository extends JpaRepository<Recinto, Integer> {
     List<Recinto> findAll();
-    @Query(value = "SELECT COUNT(a.animal_id) AS n_estancia FROM recinto r LEFT JOIN animal a ON a.recinto_id = r.recinto_id where r.recinto_id=?1 GROUP BY r.recinto_id", nativeQuery =  true)
+    @Query(value = "SELECT COUNT(a.animal_id) AS n_estancia " +
+            "FROM recinto r " +
+            "LEFT JOIN animal a ON a.recinto_id = r.recinto_id " +
+            "LEFT JOIN estancia e on a.animal_id = e.animal_id " +
+            "where r.recinto_id=?1 " +
+            "and e.estancia_id = (SELECT MAX(e2.estancia_id) from estancia e2 where e2.animal_id = a.animal_id) " +
+            "and e.fecha_salida IS NULL " +
+            "GROUP BY r.recinto_id;", nativeQuery =  true)
     int cantidadEstancias(int id);
 }

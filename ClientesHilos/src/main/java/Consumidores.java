1bf0d36faae8fc3f746e001bab3335693c483726
Ibai <ibai.lopez@alumni.mondragon.edu>
2022-06-10 12:03:44 +0200

    penis

--- ClientesHilos/src/main/java/Consumidores.java
+++ ClientesHilos/src/main/java/Consumidores.java
@@ -10,65 +10,40 @@ import java.util.concurrent.*;
 public class Consumidores {
     public static String access_token = "";
     public static String refresh_token = "";
-    public static final int NUM_CLIENTE = 200;
+    public static final int NUM_CLIENTE = 1000;
     ExecutorService executor;
     List<HiloConsumidor> tareas;
-    List<Future<Double>> lista;
-    List<Double> maximos;
+    List<Future<String>> lista;
+    List<String> maximos;
+
 
     public Consumidores() throws InterruptedException {
-        login();
         executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         tareas= new ArrayList<>();
         maximos=new ArrayList<>();
         for (int i = 0; i< NUM_CLIENTE; i++) {
             tareas.add(new HiloConsumidor());
         }
-
+        Long principio=System.currentTimeMillis();
         lista = executor.invokeAll(tareas) ;
         executor.shutdown();
-        for (Future<Double> item: lista){
+        for (Future<String> item: lista){
 
             try {
                 maximos.add(item.get());
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
-
         }
-        executor.awaitTermination(2000000, TimeUnit.SECONDS);
-        System.out.println("Tiempo medio esperado: "+ getAverage(maximos)+" ms");
+        Long fin=System.currentTimeMillis();
+        long tiempo= (fin-principio)/1000;
+        System.out.println("Tiempo: "+tiempo+" s");
+        executor.awaitTermination(900, TimeUnit.SECONDS);
 
 
     }
-    private static double getAverage(List<Double> list) {
-        return list.stream()
-                .mapToDouble(a -> a)
-                .average().orElse(0);
-    }
 
-    private void login() {
-        Form form = new Form();
-        form.add("username", "worker");
-        form.add("password","worker");
 
-        String jsonToken = RestController.RESTpostRequestLogin("/login", form);
-        if (!jsonToken.equals("")) {
-            JSONObject obj = null;
-            String access_token = "", refresh_token = "";
-            try {
-                obj = new JSONObject(jsonToken);
-                Consumidores.access_token = obj.getString("access_token");
-                Consumidores.refresh_token = obj.getString("refresh_token");
-                System.out.println("Sesion iniciada");
-            } catch (JSONException e) {
-                System.out.println("Error Parsing tokens");
-            }
-        } else {
-            System.out.println("Usuario no válido");
-
-        }
-    }
 
     public static void restartProgram() throws IOException {
         System.exit(0);
--- ClientesHilos/src/main/java/HiloConsumidor.java
+++ ClientesHilos/src/main/java/HiloConsumidor.java
@@ -1,9 +1,24 @@
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.api.representation.Form;
 import elementos.Animal;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.Callable;
 
-public class HiloConsumidor implements Callable<Double> {
+public class HiloConsumidor implements Callable<String> {
+    private static final String REST_SERVICE_URL = "https://musker.duckdns.org/api";
+    private static Client client;
 
     List<Long> listaTiempos;
 
@@ -12,23 +27,109 @@ public class HiloConsumidor implements Callable<Double> {
     }
 
     @Override
-    public Double call() {
-        for(int i=0;i<10;i++){
-            Long principio=System.currentTimeMillis();
-            String animal = RestController.RESTgetRequest("/animals/all", new HashMap<>(), String.class);
-            Long fin=System.currentTimeMillis();
-            Long tiempo= fin-principio;
-            listaTiempos.add(tiempo);
-            //System.out.println("Tiempo: " +tiempo+" ms");
+    public String call() {
+            login();
+            String animal=RESTgetRequest("/animals/all", new HashMap<>(), String.class);
             //System.out.println(animal);
-        }
-        return getAverage(listaTiempos);
+            return animal;
 
 
     }
-    private static double getAverage(List<Long> list) {
-        return list.stream()
-                .mapToLong(a -> a)
-                .average().orElse(0);
+    public static <T> T RESTgetRequest(String path, HashMap<String, Object> headers, Class<T> returnClass){
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);  // <----- set the json configuration POJO MAPPING for JSON reponse paring
+        client = Client.create(clientConfig);
+
+        headers.put("Authorization", "Bearer " + Consumidores.access_token);
+
+        WebResource webResource = client.resource(REST_SERVICE_URL)
+                .path(path);
+        WebResource.Builder tmp = null;
+        for (Map.Entry<String, Object> entry : headers.entrySet()) {
+            tmp = webResource.header(entry.getKey(), entry.getValue());
+        }
+
+        ClientResponse clientResponse = tmp.accept(MediaType.APPLICATION_JSON)
+                .get(ClientResponse.class);
+        MultivaluedMap<String, String> response = clientResponse.getHeaders();
+        if(response.containsKey("error")){
+            String errorValue = String.valueOf(response.get("error"));
+            if(errorValue.contains("Token has expired")){
+                if(refreshToken().equals("")){
+                    return RESTgetRequest(path, headers, returnClass);
+                }else if(refreshToken().equals("expired")){
+                    try {
+                        Consumidores.restartProgram();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return clientResponse.getEntity(returnClass);
+    }
+
+    private static String refreshToken(){
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);  // <----- set the json configuration POJO MAPPING for JSON reponse paring
+        client = Client.create(clientConfig);
+
+        WebResource webResource = client.resource(REST_SERVICE_URL)
+                .path("user/refresh");
+        ClientResponse clientResponse = webResource.accept(MediaType.APPLICATION_JSON).header("Authorization", "Bearer " + Consumidores.refresh_token).get(ClientResponse.class);
+        MultivaluedMap<String, String> response = clientResponse.getHeaders();
+        if(response.containsKey("error")){
+            return "expired";
+        }else{
+            String jsonResponse = clientResponse.getEntity(String.class);
+            JSONObject obj = null;
+            String access_token = "";
+            try {
+                obj = new JSONObject(jsonResponse);
+                access_token = obj.getString("access_token");
+            } catch (JSONException ex) {
+                ex.printStackTrace();
+            }
+            Consumidores.access_token = access_token;
+        }
+        return "";
+    }
+    private void login() {
+        Form form = new Form();
+        form.add("username", "worker");
+        form.add("password","worker");
+
+        String jsonToken = RESTpostRequestLogin("/login", form);
+        if (!jsonToken.equals("")) {
+            JSONObject obj = null;
+            String access_token = "", refresh_token = "";
+            try {
+                obj = new JSONObject(jsonToken);
+                Consumidores.access_token = obj.getString("access_token");
+                Consumidores.refresh_token = obj.getString("refresh_token");
+                System.out.println("Sesion iniciada");
+            } catch (JSONException e) {
+                System.out.println("Error Parsing tokens");
+            }
+        } else {
+            System.out.println("Usuario no válido");
+
+        }
+    }
+    public static String RESTpostRequestLogin(String path, Form form){
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);  // <----- set the json configuration POJO MAPPING for JSON reponse paring
+        client = Client.create(clientConfig);
+
+        WebResource webResource = client.resource(REST_SERVICE_URL)
+                .path(path);
+        ClientResponse clientResponse = webResource.type(MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class, form);
+        String result = clientResponse.getEntity(new GenericType<>(String.class));
+        if(result.contains("access_token")){
+            return result;
+        }else{
+            return "";
+        }
     }
 }
